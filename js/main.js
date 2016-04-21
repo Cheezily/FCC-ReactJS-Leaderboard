10c550f91d55f689828b946e9754448da66e6185
cheezily <pm655@yahoo.com>
2016-04-20 19:01:21 -0500

    render React components

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 var Output = React.createClass({
+  getInitialState: function() {
+    return {}
+  },
+  componentDidMount: function() {
+/*
+    this.serverRequest = $.get(this.props.url, function(data) {
+      this.setState(data).bind(this);
+      console.log(this.state);
+    })
+    */
+  },
   render: function() {
-    return <h2>Hello</h2>
+    console.log(this.props.list);
+
+    var userlist = this.props.list;
+    return (
+      <div class="snot">
+        {userlist.map(function(camper, key) {
+          return (<div key={key + 1} class='item'><span class='rank'>{key + 1}</span>
+            <span class='name'>{camper.username}<img src={camper.img} /></span>
+            <span class='alltime'>{camper.alltime}</span>
+            <span class='recent'>{camper.recent}</span>
+            </div>);
+        })}
+      </div>
+    )
   }
 });
 
-ReactDOM.render(<Output />, document.getElementById('output'));
+//ReactDOM.render(<Output list=''/>, document.getElementById('output'));
+
+$.get({
+  url: 'https://fcctop100.herokuapp.com/api/fccusers/top/recent',
+  dataType: "json",
+  success: function(data) {
+    ReactDOM.render(<Output list={data} />, document.getElementById('output'));
+    console.log(data);
+  }
+})
