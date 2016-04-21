592938c3d873380ac34e9a58c248e1571edaf5a5
cheezily <pm655@yahoo.com>
2016-04-21 11:48:56 -0500

    add functions to switch between alltime and recent lists

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var Output = React.createClass({
     return {};
   },
   render: function() {
-    console.log(this.props.list);
+    //console.log(this.props.list);
 
     var userlist = this.props.list;
     return (
@@ -23,13 +23,31 @@ var Output = React.createClass({
   }
 });
 
-//ReactDOM.render(<Output list=''/>, document.getElementById('output'));
 
-$.get({
-  url: 'https://fcctop100.herokuapp.com/api/fccusers/top/recent',
-  dataType: "json",
-  success: function(data) {
-    ReactDOM.render(<Output list={data} />, document.getElementById('output'));
-    console.log(data);
-  }
-})
+function getList(url) {
+  $.get({
+    url: url,
+    dataType: "json",
+    success: function(data) {
+      ReactDOM.render(<Output list={data} />, document.getElementById('output'));
+      //console.log(data);
+    }
+  });
+}
+
+
+$('.alltime').click(function() {
+  $('#alltimeLabel').addClass('active');
+  $('#recentLabel').removeClass('active');
+  getList('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+});
+
+
+$('.recent').click(function() {
+  $('#recentLabel').addClass('active');
+  $('#alltimeLabel').removeClass('active');
+  getList('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+});
+
+
+getList('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
